8248ab043c66bc26712d34918dfbb0eddc0dfeb6
Connor <61102529+Metater@users.noreply.github.com>
2021-09-10 21:33:35 -0400

    Add project files.

---
+++ FactioBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactioBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
